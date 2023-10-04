86b4c72c2c9a70e2231823b1e633cf57a3694676
Miha Šafranko <miha.safranko@gmail.com>
2023-10-04 18:25:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+$public_ip = '92.53.152.129';
+
+
+echo $ip;
+
+if ($ip === $public_ip) { // Check if the local IP matches
+    header('Location: http://192.168.0.111:6420');
+    exit();
+} else {
+    header('Location: http://'.$public_ip.':6420/');
+    exit();
+}
+?>
